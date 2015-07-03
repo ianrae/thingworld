724f731ebbece37a66cd25d137a4d5731507a4e2
ianrae <winfell808@gmail.com>
2015-07-03 14:19:54 -0400

    more

--- code/test/integration/ConfigurationTests.java
+++ code/test/integration/ConfigurationTests.java
@@ -5,6 +5,9 @@ import static org.junit.Assert.*;
 import java.util.HashMap;
 
 import org.junit.Test;
+import org.thingworld.config.Config;
+import org.thingworld.config.IConfig.ConfigItem;
+import org.thingworld.config.PlayConfig;
 
 import play.Application;
 import play.Configuration;
@@ -21,11 +24,18 @@ public class ConfigurationTests
 		java.util.Map<String,String> additionalConfiguration = new HashMap<>();
 		String val = "value1";
 		additionalConfiguration.put("ebean.default", val);
+		additionalConfiguration.put("COMMIT_CACHE_SEGMENT_SIZE", "12");
 		
 		Application fakeApp = Helpers.fakeApplication(additionalConfiguration);
 		Configuration config = fakeApp.configuration();
 		String s = config.getString("ebean.default");
 		assertEquals("value1", s);
+		
+		//our config uses the Play Configuration object
+		PlayConfig playConfig = new PlayConfig(config);
+		Config.setConfig(playConfig);
+		int n = Config.getIntValue(ConfigItem.COMMIT_CACHE_SEGMENT_SIZE);
+		assertEquals(12, n);
 	}
 
 }
