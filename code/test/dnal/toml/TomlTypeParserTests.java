5eee6a9e30ed2cad45fb40a99969de501784b23c
ianrae <winfell808@gmail.com>
2016-03-26 16:05:51 -0400

    more

--- code/test/dnal/toml/TomlTypeParserTests.java
+++ code/test/dnal/toml/TomlTypeParserTests.java
@@ -53,7 +53,7 @@ public class TomlTypeParserTests extends BaseTest {
 			String key = "TYPE." + dtype.name;
 			Toml toml = rootToml.getTable(key);
 			String s = toml.getString("BASE");
-			dtype.baseType = s;
+			dtype.baseType = (s == null) ? "struct" : s;
 
 			List<String> membL = toml.getList("MEMBERS");
 			for(String field: membL) {
@@ -125,22 +125,30 @@ public class TomlTypeParserTests extends BaseTest {
 		assertEquals(true, b);
 		checkSize(1, scanner.getDTypes());
 		checkEntrySize(2, scanner.getDTypes().get(0).entries);
-		checkDType(scanner.getDTypes().get(0), "struct", "Position");
+		checkDType(scanner, 0, "struct", "Position");
+		checkEntry(scanner, 0, 0, "int", "x");
+		checkEntry(scanner, 0, 1, "int", "y");
 	}
-	
-//	@Test
-//	public void testF2() {
-//		List<String> fileL = buildFile(2);
-//
-//		ParseErrorTracker errorTracker = new ParseErrorTracker();
-//		ITypeFileScanner scanner = new TypeFileScanner(errorTracker);
-//		boolean b = scanner.scan(fileL);
-//		assertEquals(true, b);
-//		checkSize(1, scanner.getDTypes());
-//		checkDTypeEntry(scanner.getDTypes().get(0).entries.get(0), "int", "size");
-//		checkDType(scanner.getDTypes().get(0), "int", "Timeout");
-//	}
-//	
+
+	@Test
+	public void testF2() {
+		List<String> fileL = buildFile(2);
+
+		ITypeFileScanner scanner = createScanner();
+		boolean b = scanner.scan(fileL);
+		assertEquals(true, b);
+		checkSize(2, scanner.getDTypes());
+		checkEntrySize(2, scanner.getDTypes().get(0).entries);
+		int i = 0;
+		checkDType(scanner, i, "struct", "Position");
+		checkEntry(scanner, i, 0, "int", "x");
+		checkEntry(scanner, 0, 1, "int", "y");
+
+		i = 1;
+		checkDType(scanner, i, "struct", "Person");
+		checkEntry(scanner, i, 0, "string", "firstName");
+		checkEntry(scanner, i, 1, "string", "lastName");
+	}
 //	@Test
 //	public void testF4() {
 //		List<String> fileL = buildFile(4);
@@ -186,6 +194,19 @@ public class TomlTypeParserTests extends BaseTest {
 //	}
 //	
 	
+	private void checkEntry(ITypeFileScanner scanner, int i, int j,
+			String string, String string2) {
+		
+		assertEquals(string, scanner.getDTypes().get(i).entries.get(j).type);
+		assertEquals(string2, scanner.getDTypes().get(i).entries.get(j).name);
+		
+	}
+
+	private void checkDType(ITypeFileScanner scanner, int i, String string,
+			String string2) {
+		checkDType(scanner.getDTypes().get(i), string, string2);
+	}
+
 	private void checkSize(int expectedSize, List<DType> list) {
 		assertEquals(expectedSize, list.size());
 	}
@@ -228,7 +249,7 @@ public class TomlTypeParserTests extends BaseTest {
 			add("]");
 			add("");
 			add("[TYPE.Person]");
-			add("BASE = 'struct'");
+//			add("BASE = 'struct'");
 			add("MEMBERS = [");
 			add("'string firstName',");
 			add("'string lastName'");
