746121f40dde7a1e17cf05d3165438e5025ede55
ianrae <winfell808@gmail.com>
2016-04-02 11:32:34 -0400

    better

--- code/test/dnal/NashornJSTests.java
+++ code/test/dnal/NashornJSTests.java
@@ -13,6 +13,55 @@ import org.junit.Test;
 import testhelper.BaseTest;
 
 public class NashornJSTests extends BaseTest {
+	
+	public static class EvalResult {
+		public boolean success;
+		public boolean result;
+		public String errMsg;
+		
+		public void succeeded(boolean result) {
+			this.success = true;
+			this.result = result;
+			this.errMsg = null;
+		}
+		public void failed(String errMsg) {
+			this.success = false;
+			this.result = false;
+			this.errMsg = errMsg;
+		}
+	}
+
+	public static class ExpressionEval {
+
+		public EvalResult evalInt(int value, String expr) {
+
+			ScriptEngineManager engineManager = new ScriptEngineManager();
+			ScriptEngine engine = engineManager.getEngineByName("nashorn");
+
+			ScriptContext context = engine.getContext();
+			context.setAttribute("value", value, ScriptContext.ENGINE_SCOPE);
+
+			if (! expr.endsWith(";")) {
+				expr += ";";
+			}
+			
+			EvalResult result = new EvalResult();
+			Object obj = null;
+			String errMsg = null;
+			try {
+				obj = engine.eval(expr);
+			} catch (ScriptException e) {
+				errMsg = e.getMessage();
+			}
+			if (obj instanceof Boolean) {
+				result.succeeded((Boolean)obj);
+			} else {
+				result.failed(errMsg);
+			}
+			
+			return result;
+		}
+	}
 
 	@Test
 	public void test0() throws Exception {
@@ -21,7 +70,7 @@ public class NashornJSTests extends BaseTest {
 		ScriptEngine engine = 
 				engineManager.getEngineByName("nashorn");
 		engine.eval("function sum(a, b) { return a + b; }");
-		
+
 		Object obj = engine.eval("sum(1, 2);");
 		log(obj.toString());
 		String s = obj.toString();
@@ -34,13 +83,36 @@ public class NashornJSTests extends BaseTest {
 				new ScriptEngineManager();
 		ScriptEngine engine = 
 				engineManager.getEngineByName("nashorn");
-		
-		 ScriptContext context = engine.getContext();
-        context.setAttribute("x", 15, ScriptContext.ENGINE_SCOPE);
-		
+
+		ScriptContext context = engine.getContext();
+		context.setAttribute("x", 15, ScriptContext.ENGINE_SCOPE);
+
 		Object obj = engine.eval("x > 0;");
 		log(obj.toString());
 		String s = obj.toString();
 		assertEquals("true", s);
+	}
+
+	@Test
+	public void test2() throws Exception {
+
+		ExpressionEval eval = new ExpressionEval();
+
+		checkEval(true, eval, 45, "value > 0");
+		checkEval(true, eval, 45, "value > 44");
+		checkEval(false, eval , 45, "value > 100");
+
+		checkEvalFail(false, eval , 45, "value >>>> 100");
+		
+	}
+	
+	private void checkEval(boolean expected, ExpressionEval eval, int val, String expr) {
+		EvalResult result = eval.evalInt(val, expr);
+		assertEquals(expected, result.result);
+	}
+	private void checkEvalFail(boolean expected, ExpressionEval eval, int val, String expr) {
+		EvalResult result = eval.evalInt(val, expr);
+		log(result.errMsg);
+		assertEquals(false, result.success);
 	}
 }
